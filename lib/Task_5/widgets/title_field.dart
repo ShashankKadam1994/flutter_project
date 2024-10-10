d51b0b1979f645a0ebf493e98be20fcea9d1f44e
RutvalThongeModern <rutvalthonge4312@gmail.com>
2024-10-10 14:07:25 +0530

    Fix: TextController value issue

--- lib/Task_5/widgets/title_field.dart
+++ lib/Task_5/widgets/title_field.dart
@@ -3,12 +3,13 @@ import 'package:flutter/services.dart';
 
 class TitleField extends StatelessWidget {
   final void Function(String) onSaved;
-
-  const TitleField({super.key, required this.onSaved});
+  final TextEditingController controller;
+  const TitleField({super.key, required this.controller, required this.onSaved});
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      controller: controller,
       keyboardType: TextInputType.text,
       decoration: InputDecoration(
         labelText: 'Blog Title Here',
