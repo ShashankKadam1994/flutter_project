83f90aa82afb50f7d5b7a8d48114b32180d95cfc
shashank kadam <shashankkadam278@gmail.com>
2024-11-05 19:30:02 +0530

    firebase plugin

--- lib/main.dart
+++ lib/main.dart
@@ -14,20 +14,20 @@ void main() async {
   // await Firebase.initializeApp();
 
 
-  if (kIsWeb) {
-    await Firebase.initializeApp(
-      options: const FirebaseOptions(
-        apiKey: "your-api-key",
-        authDomain: "your-auth-domain",
-        projectId: "your-project-id",
-        storageBucket: "your-storage-bucket",
-        messagingSenderId: "your-messaging-sender-id",
-        appId: "your-app-id",
-      ),
-    );
-  } else {
-    await Firebase.initializeApp();
-  }
+  // if (kIsWeb) {
+  //   await Firebase.initializeApp(
+  //     options: const FirebaseOptions(
+  //       apiKey: "your-api-key",
+  //       authDomain: "your-auth-domain",
+  //       projectId: "your-project-id",
+  //       storageBucket: "your-storage-bucket",
+  //       messagingSenderId: "your-messaging-sender-id",
+  //       appId: "your-app-id",
+  //     ),
+  //   );
+  // } else {
+  //   await Firebase.initializeApp();
+  // }
   // await MyDataBase().initializedDB();
   // await SessionManager().init();
   runApp(const MyApp());
